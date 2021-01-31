ed71bfd240cf9e3e52ffbc7a266dbfb35201c145
Kiko Quesada <franciscoquesadacabello89@gmail.com>
2021-01-31 11:13:10 +0100

    feat: register form

--- configs/routes.config.js
+++ configs/routes.config.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const usersController = require('../controllers/users.controller');
 
 router.get('/', (req, res, next) => {
     res.render('index');
 })
+
+router.get('/register', usersController.register);
 
 module.exports = router;
---
+++ controllers/users.controller.js
@@ -0,0 +1,5 @@
+
+
+module.exports.register = (req, res, next) => {
+    res.render('users/register');
+};
